fd3fe5c4ab1f858a193e0c6ffd75fe49980abcab
FrankCP67 <fcoaguilap@uni.pe>
2021-07-24 16:33:44 +0000

    initial commit

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,12 @@
-from simpletransformers.classification import ClassificationModel
+from simpletransformers.ner import NERModel,NERArgs
 
-model = ClassificationModel(
-    "bert", "outputs"
-)
+args = NERArgs()
+args.num_train_epochs = 10
+args.learning_rate = 1e-4
+args.overwrite_output_dir =True
+args.train_batch_size = 32
+args.eval_batch_size = 32
 
+label=['B-ORG', 'O', 'B-LOC', 'I-LOC', 'B-PER', 'I-PER', 'I-ORG']
+
+model = NERModel('bert', "outputs" ,labels=label,args =args,use_cuda=False)
